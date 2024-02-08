a28d02da83ee04f21ede19d5a5fecb47ff321a01
Naman Bhalla <naman@Namans-MacBook-Pro.local>
2024-02-08 08:08:36 +0530

    Added calculator support

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+class Calculator {
+    private int ans;
+
+    public int add(int a) {
+        ans += a;
+        return ans;
+    }
+
+    public int sub(int b) {
+        ans -= a;
+        return ans;
+    }
+}
